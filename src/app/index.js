e9ed76c29494a1068dae2d1f6353facb119648b6
Manwe <mathieu.jourdan.zola@gmail.com>
2024-09-26 17:32:16 +0200

    Add files to setup project, can't display react component yet

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './index.css';
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//const root = createRoot(rootElement);
+
+root.render(<h1>Hello, world!</h1>);
+
+//<App />

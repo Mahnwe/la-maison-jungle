582a626581da925c611698c2f77e97de43f4ad02
Manwe <mathieu.jourdan.zola@gmail.com>
2024-10-02 18:17:16 +0200

    Add style and image to the banner component

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,6 +1,15 @@
+import React from "react";
+import styles from "../components-style/Banner.css";
+
+const title = 'La maison jungle'
+
 function Banner() {
-	const title = 'La maison jungle'
-	return <h1>{title}</h1>
+
+	return ( <div className="lmj-banner">
+				{<img src="assets/leaf-logo.png" alt='La maison jungle' className='lmj-logo' />}
+				<h1>{title}</h1>
+			</div>
+	)
 }
 
 export default Banner
--- src/app/components/Cart.js
+++ src/app/components/Cart.js
@@ -1,7 +1,8 @@
+const monsteraPrice = 8
+const ivyPrice = 10
+const flowerPrice = 15
+
 function Cart() {
-	const monsteraPrice = 8
-	const ivyPrice = 10
-	const flowerPrice = 15
 	return (
 		<div>
 			<h2>Panier</h2>

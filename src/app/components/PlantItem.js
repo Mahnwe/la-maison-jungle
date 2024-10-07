5d16ba28963438bd16f5e627243e54c624c6667e
Manwe <mathieu.jourdan.zola@gmail.com>
2024-10-07 16:21:55 +0200

    Add two alert for water and light parameters of plant item

--- src/app/components/PlantItem.js
+++ src/app/components/PlantItem.js
@@ -1,18 +1,48 @@
+'use client'
+
 import CareScale from "./CareScale";
 import '../components-style/PlantItem.css'
 
 function PlantItem({id, cover, name, water, light}) 
 {
     return (
-            <li key={id} className="lmj-plant-item">
+            <li key={id} className="lmj-plant-item" onClick={() => handleClick(name)}>
                 <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			    {name}
-                <div>
+                <div onClick = { () => clickOnWaterCare(water)}>
                     <CareScale careType='water' scaleValue={water} />
+                </div>
+                <div onClick = { () => clickOnLightCare(light)}>
                     <CareScale careType='light' scaleValue={light} />
                 </div>
             </li>
         )
+}
+
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
+
+function clickOnWaterCare(water) {
+    switch(water) {
+        case 1:
+            return alert(`Cette plante nécessite peu d'arrosage`)
+        case 2:
+            return alert(`Cette plante nécessite un arrosage modéré`)
+        case 3:
+            return alert(`Cette plante nécessite beaucoup d'arrosage`)
+    }
+}
+
+function clickOnLightCare(light) {
+    switch(light) {
+        case 1:
+            return alert(`Cette plante nécessite peu de lumière`)
+        case 2:
+            return alert(`Cette plante nécessite modérement de la lumière`)
+        case 3:
+            return alert(`Cette plante nécessite beaucoup de lumière`)
+    }
 }
 
 export default PlantItem

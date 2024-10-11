518aec1923bb2374dabfac7aa94fd5c910f0c080
Manwe <mathieu.jourdan.zola@gmail.com>
2024-10-11 17:24:16 +0200

    Work on Cart and shoppingList components, add new Categories component and add style

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,14 +1,22 @@
+'use client'
 import Banner from './Banner'
 import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 import Footer from './Footer'
+import React, {useState} from 'react'
+import '../components-style/App.css'
 
 function App() {
+	const [cart, updateCart] = useState([])
+
 	return (
 		<div>
+			<link rel="icon" href="assets/leaf-logo.png" type="image/x-icon"></link>
 			<Banner />
-			<ShoppingList />
-			{/* <Cart /> */}
+			<div className='lmj-layout-inner'>
+			<Cart cart={cart} updateCart={updateCart} />
+			<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
 			<Footer />
 
 		</div>
--- src/app/components/Cart.js
+++ src/app/components/Cart.js
@@ -1,21 +1,40 @@
+'use client'
+import { useState, useEffect } from 'react'
 import '../components-style/Cart.css'
 
-const monsteraPrice = 8
-const ivyPrice = 10
-const flowerPrice = 15
-
-function Cart() {
-	return (
-		<div className='lmj-cart'>
-			<h2>Panier</h2>
-			<ul>
-				<li>Monstera : {monsteraPrice}€</li>
-				<li>Lierre : {ivyPrice}€</li>
-				<li>Fleurs : {flowerPrice}€</li>
-			</ul>
-			Total : {monsteraPrice + ivyPrice + flowerPrice}€
-		</div>
-	)
+function Cart({cart, updateCart}) {
+	
+		const [isOpen, setIsOpen] = useState(false)
+		const total = cart.reduce(
+			(acc, plantType) => acc + plantType.amount * plantType.price,
+			0
+		)
+	
+		return isOpen ? (
+			<div className='lmj-cart'>
+				<button className='lmj-cart-toggle-button' onClick= {() => setIsOpen(false)}>Fermer</button>
+				{cart.length > 0 ? (
+				<div>
+					<h2 className='lmj-cart-title'>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
+						))}
+					</ul>
+					<h3 className='lmj-cart-total'>Total : {total}€</h3>
+					<button className='lmj-cart-clear' onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div className='lmj-cart-empty'>Votre panier est vide</div>
+			)}
+			</div>
+		) : (
+			<div className='lmj-cart-closed'>
+			<button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>Ouvrir le panier</button>
+			</div>
+		)
 }
 
 export default Cart

4f0fa5693a08ed7e4dcfd25734621cb1ef6dca0e
Manwe <mathieu.jourdan.zola@gmail.com>
2024-10-15 21:50:04 +0200

    Add a button that delete items in cart

--- src/app/components/Cart.js
+++ src/app/components/Cart.js
@@ -5,10 +5,45 @@ import '../components-style/Cart.css'
 function Cart({cart, updateCart}) {
 	
 		const [isOpen, setIsOpen] = useState(false)
-		const total = cart.reduce(
+		var total = cart.reduce(
 			(acc, plantType) => acc + plantType.amount * plantType.price,
 			0
 		)
+
+function deleteInCart(amount, index, name, price) 
+{
+	const deletedPlant = [...cart]
+		if (amount > 1) 
+		{
+			if (cart.length == 1) {
+				updateCart([
+					{ name, price, amount: amount - 1 }
+				])
+			}
+			if (cart.length > 1) {
+				const cartFilteredCurrentPlant = cart.filter(
+					(plant) => plant.name !== name
+				)
+				updateCart([...cartFilteredCurrentPlant,
+					{ name, price, amount: amount - 1 }
+				])
+			}
+		}
+		if (amount == 1) 
+		{
+			//console.log(cart.length)
+			if (cart.length > 1) 
+			{
+				deletedPlant.splice(index, 1)
+				updateCart(deletedPlant, total -= price)
+			}
+			else if (cart.length == 1)
+				{
+					//console.log(cart.length)
+					updateCart([], total -= price)
+				}
+		}
+}
 	
 		return isOpen ? (
 			<div className='lmj-cart'>
@@ -19,7 +54,10 @@ function Cart({cart, updateCart}) {
 					<ul>
 						{cart.map(({ name, price, amount }, index) => (
 							<div key={`${name}-${index}`}>
-								{name} {price}€ x {amount}
+								{name} {price}€ x {amount} 
+								<button className='lmj-delete-button' width={16} height={16} onClick={() => deleteInCart(amount, index, name, price)}>
+								<img src="assets/remove.png" alt='supprimer' width={16} height={16}/>
+								</button>
 							</div>
 						))}
 					</ul>
